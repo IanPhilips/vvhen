961bf7012dc10267e295ca6f57c6f2a44b774a79
Joshua Mitchell <jm@Joshuas-MacBook-Pro.local>
2017-07-15 16:30:08 -0500

    got images up and styles and js separated into their own folders

--- app.py
+++ app.py
@@ -5,9 +5,10 @@ Created on Sat Jul 15 12:38:38 2017
 @author: iansp
 """
 
+import flask
 from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return flask.render_template('splash.html')
