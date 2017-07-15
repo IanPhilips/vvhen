a0cf84c0a9561bc2dc01d8be156577e99efb1094
joshualmitchell <jm@Joshuas-MacBook-Pro.local>
2017-07-15 17:10:51 -0500

    Added raw prototype of planner page

--- app.py
+++ app.py
@@ -12,3 +12,19 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return flask.render_template('splash.html')
+
+@app.route("/vvhen")
+def vvhen():
+    return flask.render_template('planner_name.html')
+
+@app.route('/submit-planner-info', methods=['POST'])
+def new_user():
+    error = None
+    name = flask.request.form['plannername']
+    email = flask.request.form['email']
+
+    if user and email is None:
+        error = "Fields can't be blank"
+        return flask.render_template('planner_name.html', error=error)
+
+    return flask.render_template('planner_name.html')
