93f64f763c01a5741fe631f26fdbf847c63b4ff9
joshualmitchell <jm@Joshuas-MacBook-Pro.local>
2017-07-15 17:41:03 -0500

    Have page for list of invitees and master calendar prototype

--- app.py
+++ app.py
@@ -20,11 +20,15 @@ def vvhen():
 @app.route('/submit-planner-info', methods=['POST'])
 def new_user():
     error = None
-    name = flask.request.form['plannername']
+    planner_name = flask.request.form['plannername']
     email = flask.request.form['email']
 
-    if user and email is None:
+    if planner_name and email is None:
         error = "Fields can't be blank"
         return flask.render_template('planner_name.html', error=error)
 
-    return flask.render_template('planner_name.html')
+    return flask.render_template('planner_information.html', planner_name=planner_name, planner_email=planner_name)
+
+@app.route("/list-of-invitees")
+def list_of_invitees():
+    return flask.render_template('list_of_invitees.html')
